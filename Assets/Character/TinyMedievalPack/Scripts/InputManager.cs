572b4a96096ad03801099c5fc95d0cf17c49a64d
Jereth Champagne <hemorrage@live.com>
2014-11-05 13:51:59 -0600

    input manager bug fixes

--- Assets/Character/TinyMedievalPack/Scripts/InputManager.cs
+++ Assets/Character/TinyMedievalPack/Scripts/InputManager.cs
@@ -2,54 +2,50 @@
 using System.Collections;
 
 public class InputManager : MonoBehaviour {
-
-	private Vector3 forward;
-	private Vector3 back;
-	private Vector3 left;
-	private Vector3 right;
+	
 	private float moveSpeed = 3.0f;
-
+	public float Turnspeed;
+	
 	private PlayerController controller;
 	
 	// Use this for initialization
 	void Start () {
-
-		forward = transform.forward;
-		back = -transform.forward;
-		left = -transform.right;
-		right = transform.right;
-
+		
 		controller = GetComponent<PlayerController> ();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
+		
 		if (Input.GetKeyUp (KeyCode.W)) 
 		{
 			controller.ResetRun();
 		}
-
+		
 		if (Input.GetKey (KeyCode.W)) 
 		{
-			transform.forward = forward;
+			
+			//transform.forward = Camera.main.transform.forward;
 			transform.position += moveSpeed * Time.deltaTime * transform.forward;
 			controller.Run();
+			
 		}
-
+		
 		if (Input.GetKeyUp (KeyCode.S)) 
 		{
 			controller.ResetRun();
 		}
-
+		
 		if (Input.GetKey (KeyCode.S)) 
 		{
-			transform.forward = back;
-			transform.position += moveSpeed * Time.deltaTime * transform.forward;
+			
+			transform.position -= moveSpeed * Time.deltaTime * transform.forward;
 			controller.Run();
+			
 		}
-
+		
 		if (Input.GetKeyUp (KeyCode.A)) 
 		{
 			controller.ResetStrafe();
@@ -57,24 +53,30 @@ public class InputManager : MonoBehaviour {
 		
 		if (Input.GetKey (KeyCode.A)) 
 		{
-			transform.forward = left;
-			transform.position += moveSpeed * Time.deltaTime * transform.forward;
-			controller.Strafe();
+			
+			transform.Rotate(Vector3.up, -1f * Turnspeed * Time.deltaTime);
+			//transform.position -= moveSpeed * Time.deltaTime * transform.right;
+			//controller.Strafe();
+			
 		}
-
+		
 		if (Input.GetKeyUp (KeyCode.D)) 
 		{
 			controller.ResetStrafe();
 		}
-
+		
 		if (Input.GetKey (KeyCode.D)) 
 		{
-			transform.forward = right;
-			transform.position += moveSpeed * Time.deltaTime * transform.forward;
-			controller.Strafe();
+			
+			transform.Rotate(Vector3.up, 1f * Turnspeed * Time.deltaTime);
+			//transform.position += moveSpeed * Time.deltaTime * transform.right;
+			//controller.Strafe();
+			
 		}
 		
 		
 		
 	}
+	
+	
 }
